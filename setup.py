9c89c9c52901554289db11a3119a1421c644bb79
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-12 17:39:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import *
+
+libTM = Extension('libTM',
+  ['pyTsetlinMachine/ConvolutionalTsetlinMachine.c', 'pyTsetlinMachine/MultiClassConvolutionalTsetlinMachine.c', 'pyTsetlinMachine/Tools.c'])
+
+setup(
+   name='pyTsetlinMachine',
+   version='0.1.0',
+   author='Ole-Christoffer Granmo',
+   author_email='ole.granmo@uia.no',
+   url='http://pypi.python.org/pypi/pyTsetlinMachine/',
+   license='LICENSE.txt',
+   description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine, and Regression Tsetlin Machine',
+   long_description=open('README.txt').read(),
+   ext_modules = [libTM],
+   packages=['pyTsetlinMachine'],
+)
