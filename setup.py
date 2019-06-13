08d291c1c15133b9d800c6d5f83da91c5f1e0359
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-13 14:48:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ libTM = Extension('libTM',
 
 setup(
    name='pyTsetlinMachine',
-   version='0.1.5',
+   version='0.1.6',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
    url='https://github.com/cair/pyTsetlinMachine/',
    license='MIT',
    description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine and Regression Tsetlin Machine',
+   long_description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine and Regression Tsetlin Machine',
    ext_modules = [libTM],
+   keywords = "pattern-recognition machine-learning interpretable rule-based propositional-logic tsetlin-machine regression convolution",
    packages=['pyTsetlinMachine'],
 )
