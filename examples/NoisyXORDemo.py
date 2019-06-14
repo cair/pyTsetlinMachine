8aac750f90df63ec108d2ad74c96df066e8ddf3d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-14 15:00:49 +0200

    Update NoisyXORDemo.py

--- examples/NoisyXORDemo.py
+++ examples/NoisyXORDemo.py
@@ -9,7 +9,7 @@ test_data = np.loadtxt("NoisyXORTestData.txt")
 X_test = test_data[:,0:-1]
 Y_test = test_data[:,-1]
 
-tm = MultiClassTsetlinMachine(10, 15, 3.9, boost_true_positive_feedback=1)
+tm = MultiClassTsetlinMachine(10, 15, 3.9, boost_true_positive_feedback=0)
 
 tm.fit(X_train, Y_train, epochs=200)
 print("Accuracy: %.2f%%" % (100.0*tm.evaluate(X_test, Y_test)))
