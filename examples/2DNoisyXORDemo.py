eff6d19593d758e37ec47a085c1cc42cd7d834f6
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-15 08:46:09 +0200

    Update 2DNoisyXORDemo.py

--- examples/2DNoisyXORDemo.py
+++ examples/2DNoisyXORDemo.py
@@ -2,7 +2,6 @@
 
 from pyTsetlinMachine.tm import MultiClassConvolutionalTsetlinMachine2D
 import numpy as np 
-from time import time
 
 train_data = np.loadtxt("2DNoisyXORTrainingData.txt")
 X_train = train_data[:,0:-1].reshape(train_data.shape[0], 4, 4)
@@ -14,11 +13,6 @@ Y_test = test_data[:,-1]
 
 ctm = MultiClassConvolutionalTsetlinMachine2D(40, 60, 3.9, (2, 2), boost_true_positive_feedback=0)
 
-results = np.zeros(0)
-for i in range(100):
-	start = time()
-	ctm.fit(X_train, Y_train, epochs=5000)
-	stop = time()
+ctm.fit(X_train, Y_train, epochs=5000)
 
-	results = np.append(results, 100.0*ctm.evaluate(X_test, Y_test))
-	print("#%d Mean Accuracy (%%): %.2f; Std.dev.: %.2f; Training Time: %.1f ms/epoch" % (i+1, np.mean(results), np.std(results), (stop-start)/5.0))
+print("Accuracy: %.2f%%" % (100.0*ctm.evaluate(X_test, Y_test))
