96e6b65587215c4b8108b66219a80c9ff8b28d8e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-17 11:59:19 +0200

    Updated

--- examples/MNISTDemo.py
+++ examples/MNISTDemo.py
@@ -1,4 +1,4 @@
-from pyTsetlinMachine.tm import MultiClassConvolutionalTsetlinMachine2D
+from pyTsetlinMachine.tm import MultiClassTsetlinMachine
 import numpy as np
 from time import time
 
@@ -6,18 +6,18 @@ from keras.datasets import mnist
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-X_train = np.where(X_train > 75, 1, 0) 
-X_test = np.where(X_test > 75, 1, 0) 
+X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
+X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
 print(X_train[0])
 
-tm = MultiClassConvolutionalTsetlinMachine2D(2000, 50, 10.0, (10, 10))
+tm = MultiClassTsetlinMachine(2000, 50, 10.0)
 
-print("\nAccuracy over 10 epochs:\n")
+print("\nAccuracy over 100 epochs:\n")
 tm_results = np.empty(0)
-for i in range(10):
-	tm.fit(X_train, Y_train, epochs=1, incremental=True)
+for i in range(100):
 	start = time()
-	tm_results = np.append(tm_results, np.array(100*(tm.predict(X_test) == Y_test).mean()))
+	tm.fit(X_train, Y_train, epochs=1, incremental=True)
 	stop = time()
+	tm_results = np.append(tm_results, np.array(100*(tm.predict(X_test) == Y_test).mean()))
 	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, tm_results.mean(), stop-start))
