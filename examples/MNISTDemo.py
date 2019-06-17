3fe20c5d88a5335ce2bf6696d32ee6a0230b7cce
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-17 11:56:57 +0200

    MNIST Demo

---
+++ examples/MNISTDemo.py
@@ -0,0 +1,23 @@
+from pyTsetlinMachine.tm import MultiClassConvolutionalTsetlinMachine2D
+import numpy as np
+from time import time
+
+from keras.datasets import mnist
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+X_train = np.where(X_train > 75, 1, 0) 
+X_test = np.where(X_test > 75, 1, 0) 
+
+print(X_train[0])
+
+tm = MultiClassConvolutionalTsetlinMachine2D(2000, 50, 10.0, (10, 10))
+
+print("\nAccuracy over 10 epochs:\n")
+tm_results = np.empty(0)
+for i in range(10):
+	tm.fit(X_train, Y_train, epochs=1, incremental=True)
+	start = time()
+	tm_results = np.append(tm_results, np.array(100*(tm.predict(X_test) == Y_test).mean()))
+	stop = time()
+	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, tm_results.mean(), stop-start))
