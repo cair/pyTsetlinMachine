e844a1850e3ebe7015fe01e181f11d5673e17e4b
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-20 00:33:31 +0200

    Add files via upload

---
+++ examples/RegressionDemo.py
@@ -0,0 +1,29 @@
+from pyTsetlinMachine.tm import RegressionTsetlinMachine
+from pyTsetlinMachine.tools import Binarizer
+import numpy as np
+from time import time
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+california_housing = datasets.fetch_california_housing()
+X = california_housing.data
+Y = california_housing.target
+
+b = Binarizer(max_bits_per_feature = 10)
+b.fit(X)
+X_transformed = b.transform(X)
+
+tm = RegressionTsetlinMachine(4000, 2000, 2.75)
+
+print("\nRMSD over 100 runs:\n")
+tm_results = np.empty(0)
+for i in range(100):
+	X_train, X_test, Y_train, Y_test = train_test_split(X_transformed, Y)
+
+	start = time()
+	tm.fit(X_train, Y_train, epochs=30)
+	stop = time()
+	tm_results = np.append(tm_results, np.sqrt(((tm.predict(X_test) - Y_test)**2).mean()))
+
+	print("#%d RMSD: %.2f +/- %.2f (%.2fs)" % (i+1, tm_results.mean(), 1.96*tm_results.std()/np.sqrt(i+1), stop-start))
