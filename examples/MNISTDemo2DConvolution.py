9885e7c06039306c281d47fe4a77a41c4745c846
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-06-19 09:43:40 +0200

    Update MNISTDemo2DConvolution.py

--- examples/MNISTDemo2DConvolution.py
+++ examples/MNISTDemo2DConvolution.py
@@ -6,13 +6,13 @@ from keras.datasets import mnist
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-X_train = np.where(X_train > 75, 1, 0) 
-X_test = np.where(X_test > 75, 1, 0) 
+X_train = np.where(X_train >= 75, 1, 0) 
+X_test = np.where(X_test >= 75, 1, 0) 
 
-tm = MultiClassConvolutionalTsetlinMachine2D(2000, 50, 10.0, (10, 10))
+tm = MultiClassConvolutionalTsetlinMachine2D(8000, 200, 10.0, (10, 10))
 
-print("\nAccuracy over 100 epochs:\n")
-for i in range(100):
+print("\nAccuracy over 25 epochs:\n")
+for i in range(25):
 	start = time()
 	tm.fit(X_train, Y_train, epochs=1, incremental=True)
 	stop = time()
