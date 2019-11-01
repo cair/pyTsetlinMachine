065cede1b691667421d81b02c64d2ab6ac735e32
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-11-01 17:21:42 +0100

    Update MNISTDemo.py

--- examples/MNISTDemo.py
+++ examples/MNISTDemo.py
@@ -11,10 +11,14 @@ X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0)
 
 tm = MultiClassTsetlinMachine(2000, 50, 10.0)
 
-print("\nAccuracy over 200 epochs:\n")
-for i in range(200):
-	start = time()
+print("\nAccuracy over 400 epochs:\n")
+for i in range(400):
+	start_training = time()
 	tm.fit(X_train, Y_train, epochs=1, incremental=True)
-	stop = time()
+	stop_training = time()
+
+	start_testing = time()
 	result = 100*(tm.predict(X_test) == Y_test).mean()
-	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
+	stop_testing = time()
+
+	print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
