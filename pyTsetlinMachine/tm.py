3a23d1aa7952fee7e42e52af9982179effdf2f8d
Rupsa Saha <rupsajina@gmail.com>
2019-11-08 11:18:41 +0100

    Added save, load, access clauses
    MultiClassTsetlinMachine: access clauses, save, load
    MultiClassConvolutionalTsetlinMachine2D : save, load

--- pyTsetlinMachine/tm.py
+++ pyTsetlinMachine/tm.py
@@ -303,6 +303,24 @@ class MultiClassTsetlinMachine():
 
 		return
 	
+	#return list of all clauses	
+	def get_all_clauses(self,list_of_classes,num_of_clauses, num_of_features):	
+		all_clauses=[[] for i in range (num_of_clauses)]	
+		for cur_clause in range(num_of_clauses):	
+			for cur_cls in list_of_classes:	
+				this_clause=''	
+				for f in range(num_of_features*2):	
+					action = self.ta_action(int(cur_cls), cur_clause, f)	
+					if action==1:	
+						if this_clause!='':	
+							this_clause+='AND '	
+						if f<num_of_features:	
+							this_clause+='F'+str(f)+' '	
+						else:	
+							this_clause+='-|F'+str(f-num_of_features)+' '	
+				all_clauses[cur_clause].append(this_clause) #all_clauses[cur_clause][cur_cls] = this_clause	
+		return all_clauses	
+
 	#save trained model and hyperparams
 	def save_model(self, savefile):
 		save_ta_state = self.get_state()
