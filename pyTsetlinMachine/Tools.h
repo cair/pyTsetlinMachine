8ded055919f842599d11c4dd53bb41fb04038a1d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-11-19 10:48:25 +0100

    Add files via upload

--- pyTsetlinMachine/Tools.h
+++ pyTsetlinMachine/Tools.h
@@ -1,2 +1,2 @@
-void tm_encode(unsigned int *X, unsigned int *encoded_X, int number_of_examples, int dim_x, int dim_y, int dim_z, int patch_dim_x, int patch_dim_y);
+void tm_encode(unsigned int *X, unsigned int *encoded_X, int number_of_examples, int dim_x, int dim_y, int dim_z, int patch_dim_x, int patch_dim_y, int append_negated);
 
