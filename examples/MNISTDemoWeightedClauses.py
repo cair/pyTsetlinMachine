76a676aafc53de641b646cd3609005d8b19b5864
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-11-28 11:17:39 +0100

    Add files via upload

---
+++ examples/MNISTDemoWeightedClauses.py
@@ -0,0 +1,24 @@
+from pyTsetlinMachine.tm import MultiClassTsetlinMachine
+import numpy as np
+from time import time
+
+from keras.datasets import mnist
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
+X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
+
+tm = MultiClassTsetlinMachine(2000, 50*100, 10.0, weighted_clauses=True)
+
+print("\nAccuracy over 60 epochs:\n")
+for i in range(60):
+	start_training = time()
+	tm.fit(X_train, Y_train, epochs=1, incremental=True)
+	stop_training = time()
+
+	start_testing = time()
+	result = 100*(tm.predict(X_test) == Y_test).mean()
+	stop_testing = time()
+
+	print("#%d Accuracy: %.2f%% Training: %.2fs Testing: %.2fs" % (i+1, result, stop_training-start_training, stop_testing-start_testing))
