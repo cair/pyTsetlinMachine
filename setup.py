437116c83232b2ab03602f70f012a003c73833d7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-11-29 10:13:39 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ libTM = Extension('libTM',
 
 setup(
    name='pyTsetlinMachine',
-   version='0.4.4',
+   version='0.4.5',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
    url='https://github.com/cair/pyTsetlinMachine/',
    license='MIT',
-   description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and clause indexing.',
-   long_description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and clause indexing.',
+   description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features, multigranularity, and clause indexing.',
+   long_description='Implements the Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features, multigranularity, and clause indexing.',
    ext_modules = [libTM],
    keywords ='pattern-recognition machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution',
    packages=['pyTsetlinMachine']
