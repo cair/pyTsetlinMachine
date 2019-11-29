d8c9c3e6fb1a95347fae181459668c0a5d69e3aa
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-11-29 20:25:08 +0100

    Update MNISTDemo2DConvolutionWeightedClauses.py

--- examples/MNISTDemo2DConvolutionWeightedClauses.py
+++ examples/MNISTDemo2DConvolutionWeightedClauses.py
@@ -9,7 +9,7 @@ from keras.datasets import mnist
 X_train = np.where(X_train >= 75, 1, 0) 
 X_test = np.where(X_test >= 75, 1, 0) 
 
-tm = MultiClassConvolutionalTsetlinMachine2D(1000, 25*100, 10.0, (10, 10), weighted_clauses=True)
+tm = MultiClassConvolutionalTsetlinMachine2D(2000, 50*100, 10.0, (10, 10), weighted_clauses=True)
 
 print("\nAccuracy over 100 epochs:\n")
 for i in range(100):
