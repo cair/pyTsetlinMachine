dfaa7f36a5fa5cc852645277605358ae4d955898
Ole-Christoffer Granmo <ole.granmo@uia.no>
2019-12-08 19:37:12 +0100

    Add files via upload

--- examples/InterpretabilityDemo.py
+++ examples/InterpretabilityDemo.py
@@ -1,11 +1,14 @@
 from pyTsetlinMachine.tm import MultiClassTsetlinMachine
 import numpy as np 
 
-X_train = np.random.randint(0, 2, size=(5000, 12), dtype=np.uint32)
-Y_train = np.logical_xor(X_train[:,0], X_train[:,1]).astype(dtype=np.uint32)
-Y_train = np.where(np.random.rand(5000) < 0.1, 1-Y_train, Y_train) # Adds 10% noise
+number_of_features = 20
+noise = 0.1
 
-X_test = np.random.randint(0, 2, size=(5000, 12), dtype=np.uint32)
+X_train = np.random.randint(0, 2, size=(5000, number_of_features), dtype=np.uint32)
+Y_train = np.logical_xor(X_train[:,0], X_train[:,1]).astype(dtype=np.uint32)
+Y_train = np.where(np.random.rand(5000) <= noise, 1-Y_train, Y_train) # Adds noise
+
+X_test = np.random.randint(0, 2, size=(5000, number_of_features), dtype=np.uint32)
 Y_test = np.logical_xor(X_test[:,0], X_test[:,1]).astype(dtype=np.uint32)
 
 tm = MultiClassTsetlinMachine(10, 15, 3.0, boost_true_positive_feedback=0)
@@ -18,22 +21,46 @@ print("\nClass 0 Positive Clauses:\n")
 for j in range(0, 10, 2):
 	print("Clause #%d: " % (j), end=' ')
 	l = []
-	for k in range(24):
+	for k in range(number_of_features*2):
 		if tm.ta_action(0, j, k) == 1:
-			if k < 12:
+			if k < number_of_features:
 				l.append(" x%d" % (k))
 			else:
-				l.append("¬x%d" % (k-12))
+				l.append("¬x%d" % (k-number_of_features))
+	print(" ∧ ".join(l))
+
+print("\nClass 0 Negative Clauses:\n")
+for j in range(1, 10, 2):
+	print("Clause #%d: " % (j), end=' ')
+	l = []
+	for k in range(number_of_features*2):
+		if tm.ta_action(0, j, k) == 1:
+			if k < number_of_features:
+				l.append(" x%d" % (k))
+			else:
+				l.append("¬x%d" % (k-number_of_features))
 	print(" ∧ ".join(l))
 
 print("\nClass 1 Positive Clauses:\n")
 for j in range(0, 10, 2):
 	print("Clause #%d: " % (j), end=' ')
 	l = []
-	for k in range(24):
+	for k in range(number_of_features*2):
 		if tm.ta_action(1, j, k) == 1:
-			if k < 12:
+			if k < number_of_features:
 				l.append(" x%d" % (k))
 			else:
-				l.append("¬x%d" % (k-12))
+				l.append("¬x%d" % (k-number_of_features))
+	print(" ∧ ".join(l))
+
+print("\nClass 1 Negative Clauses:\n")
+for j in range(1, 10, 2):
+	print("Clause #%d: " % (j), end=' ')
+	l = []
+	for k in range(number_of_features*2):
+		if tm.ta_action(1, j, k) == 1:
+			if k < number_of_features:
+				l.append(" x%d" % (k))
+			else:
+				l.append("¬x%d" % (k-number_of_features))
 	print(" ∧ ".join(l))
