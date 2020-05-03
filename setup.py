d335612c75beef8bfd0b53b3a991c2aabdf82149
Ole-Christoffer Granmo <ole.granmo@uia.no>
2020-05-03 13:32:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ libTM = Extension('libTM',
 
 setup(
    name='pyTsetlinMachine',
-   version='0.5.8',
+   version='0.5.9',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
    url='https://github.com/cair/pyTsetlinMachine/',
