127c37f3ff12b9ce9f7903619202c9f925f02787
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-05-30 23:28:03 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
-import mock
-
 # -- Project information -----------------------------------------------------
 
 project = 'pyTsetlinMachine'
