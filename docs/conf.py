b1484b50d3b5bc9cfa75a71a41f981bf067f35e6
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-05-30 23:27:39 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,6 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'ctypes']
-for mod_name in MOCK_MODULES:
-  sys.modules[mod_name] = mock.Mock()
-
 # -- Project information -----------------------------------------------------
 
 project = 'pyTsetlinMachine'
