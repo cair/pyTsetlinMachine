e3c4e97b7f3c07d477913a0d0c9803a271a3fb43
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-10-12 13:02:22 +0200

    Update MultiClassConvolutionalTsetlinMachine.c

--- pyTsetlinMachine/MultiClassConvolutionalTsetlinMachine.c
+++ pyTsetlinMachine/MultiClassConvolutionalTsetlinMachine.c
@@ -136,14 +136,12 @@ void mc_tm_update(struct MultiClassTsetlinMachine *mc_tm, unsigned int *Xi, int 
 
 static void shuffle(int *array, size_t n)
 {
-	if (n > 1) {
-		size_t i;
-		for (i = 0; i < n - 1; i++) {
-			size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-			int t = array[j];
-			array[j] = array[i];
-			array[i] = t;
-		}
+	size_t i;
+	for (i = 0; i < n - 1; i++) {
+		size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+		int t = array[j];
+		array[j] = array[i];
+		array[i] = t;
 	}
 }
 
